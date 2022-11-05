da05bc04c407f61eec6fbb55f5f86290a0fe2c11
veden <stud0000248921@study.utmn.ru>
2022-11-06 02:50:10 +0500

    index.php ready

--- index.php
+++ index.php
@@ -46,7 +46,7 @@
             </div>
         </div>
     </header>
-    <!-- carousel  -->
+    <!-- carousel -->
     <div class="container">
         <div class="row">
             <h2 class="carousel_title">Карусель</h2>
@@ -57,39 +57,219 @@
 <!--            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>-->
 <!--            <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>-->
 <!--        </div>-->
-        <div class="carousel-inner">
-            <div class="carousel-item active">
-                <img src="images/image1carousel.jpg" class="d-block w-100" height="400"  alt="...">
-                <div class="carousel-caption-hack carousel-caption d-none d-md-block">
-                    <h5><a href="#">111</a></h5>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="images/image1carousel.jpg" class="d-block w-100" height="400"  alt="...">
+                    <div class="carousel-caption-hack carousel-caption d-none d-md-block">
+                        <h5><a href="#">111</a></h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/image2carousel.jpg" class="d-block w-100" height="400"  alt="...">
+                    <div class="carousel-caption-hack  carousel-caption d-none d-md-block">
+                        <h5><a href="#">111</a></h5>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="images/image3carousel.jpeg" class="d-block w-100" height="400"  alt="...">
+                    <div class="carousel-caption-hack  carousel-caption d-none d-md-block">
+                        <h5><a href="#">111</a></h5>
+                    </div>
                 </div>
             </div>
-            <div class="carousel-item">
-                <img src="images/image2carousel.jpg" class="d-block w-100" height="400"  alt="...">
-                <div class="carousel-caption-hack  carousel-caption d-none d-md-block">
-                    <h5><a href="#">111</a></h5>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+            </button>
+        </div>
+    </div>
+   <!-- carousel end -->
 
+    <!-- main -->
+    <div class="container">
+        <div class="content row">
+            <!-- main  -->
+            <div class="main-content col-md-9 col-12">
+                <h2>Последние публикации</h2>
+
+                <div class="post row">
+                    <div class="post_img col-12 col-md-4">
+                        <img src="images/Screenshot_1.png" alt="post" class="img-thumbnail">
+                    </div>
+                    <div class="post_text col-12 col-md-8">
+                        <h3>
+                            <a href="#">Статья по эйфилеву башню</a>
+                        </h3>
+                        <i class="fa fa-user"> Автор</i>
+                        <i class="fa-regular fa-calendar-days"> Дата</i>
+                        <p class="preview-text">
+                            струна, приют, принц, аккомпанемент, ртуть,
+                            лицо, строгий, образование, отступать, японец
+                            ложить, колун, гнев, высочество, исправлять
+                            компетентный, негатив, престол, изменять, консилиум
+                        </p>
+                    </div>
                 </div>
+                <div class="post row">
+                    <div class="post_img col-12 col-md-4">
+                        <img src="images/image2carousel.jpg" alt="post" class="img-thumbnail">
+                    </div>
+                    <div class="post_text col-12 col-md-8">
+                        <h3>
+                            <a href="#">Статья по эйфилеву башню</a>
+                        </h3>
+                        <i class="fa fa-user"> Автор</i>
+                        <i class="fa-regular fa-calendar-days"> Дата</i>
+                        <p class="preview-text">
+                            струна, приют, принц, аккомпанемент, ртуть,
+                            лицо, строгий, образование, отступать, японец
+                            ложить, колун, гнев, высочество, исправлять
+                            компетентный, негатив, престол, изменять, консилиум
+                        </p>
+                    </div>
+                </div>
+                <div class="post row">
+                    <div class="post_img col-12 col-md-4">
+                        <img src="images/image2carousel.jpg" alt="post" class="img-thumbnail">
+                    </div>
+                    <div class="post_text col-12 col-md-8">
+                        <h3>
+                            <a href="#">Статья по эйфилеву башню</a>
+                        </h3>
+                        <i class="fa fa-user"> Автор</i>
+                        <i class="fa-regular fa-calendar-days"> Дата</i>
+                        <p class="preview-text">
+                            струна, приют, принц, аккомпанемент, ртуть,
+                            лицо, строгий, образование, отступать, японец
+                            ложить, колун, гнев, высочество, исправлять
+                            компетентный, негатив, престол, изменять, консилиум
+                        </p>
+                    </div>
+                </div>
+                <div class="post row">
+                    <div class="post_img col-12 col-md-4">
+                        <img src="images/image2carousel.jpg" alt="post" class="img-thumbnail">
+                    </div>
+                    <div class="post_text col-12 col-md-8">
+                        <h3>
+                            <a href="#">Статья по эйфилеву башню</a>
+                        </h3>
+                        <i class="fa fa-user"> Автор</i>
+                        <i class="fa-regular fa-calendar-days"> Дата</i>
+                        <p class="preview-text">
+                            струна, приют, принц, аккомпанемент, ртуть,
+                            лицо, строгий, образование, отступать, японец
+                            ложить, колун, гнев, высочество, исправлять
+                            компетентный, негатив, престол, изменять, консилиум
+                        </p>
+                    </div>
+                </div>
+                <div class="post row">
+                    <div class="post_img col-12 col-md-4">
+                        <img src="images/image2carousel.jpg" alt="post" class="img-thumbnail">
+                    </div>
+                    <div class="post_text col-12 col-md-8">
+                        <h3>
+                            <a href="#">Статья по эйфилеву башню</a>
+                        </h3>
+                        <i class="fa fa-user"> Автор</i>
+                        <i class="fa-regular fa-calendar-days"> Дата</i>
+                        <p class="preview-text">
+                            струна, приют, принц, аккомпанемент, ртуть,
+                            лицо, строгий, образование, отступать, японец
+                            ложить, колун, гнев, высочество, исправлять
+                            компетентный, негатив, престол, изменять, консилиум
+                        </p>
+                    </div>
+                </div>
+
             </div>
-            <div class="carousel-item">
-                <img src="images/image3carousel.jpeg" class="d-block w-100" height="400"  alt="...">
-                <div class="carousel-caption-hack  carousel-caption d-none d-md-block">
-                    <h5><a href="#">111</a></h5>
+            <!-- sidebar -->
+            <div class="sidebar col-md-3 col-12">
+                <div class="section search">
+                    <h3>Поиск</h3>
+                    <form action="index.php" method="post">
+                        <input type="text" name="search-term" class="text-input" placeholder="поиск...">
+                    </form>
+                </div>
+
+                <div class="section topics">
+                    <h3>Темы</h3>
+                    <ul>
+                        <li><a href="#">Мотивация</a></li>
+                        <li><a href="#">Наука</a></li>
+                        <li><a href="#">Биография</a></li>
+                        <li><a href="#">Факты</a></li>
+                        <li><a href="#">Програмирование</a></li>
+                        <li><a href="#">Компьютеры</a></li>
+                    </ul>
                 </div>
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Previous</span>
-        </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="visually-hidden">Next</span>
-        </button>
     </div>
-    </div>
-   <!--carousel end -->
+    <!-- main end -->
+    <!-- footer -->
+    <div class="footer container-fluid">
+        <div class="footer-content container">
+            <div class="row">
+                <div class="section about col-md-4 cpl-12">
+                    <h3 class="logo-text">My blog</h3>
+                    <p>
+                        Мой блог - это лучший в мире блог струна, приют, принц, аккомпанемент, ртуть,
+                        лицо, строгий, образование, отступать, японец
+                        ложить, колун, гнев, высочество, исправлять
+                    </p>
+                    <div class="contact">
+                        <span><i class="fa-solid fa-phone"></i> 8-951-888-78-88</span>
+                        <span><i class="fa-solid fa-envelope"></i> 8-951-888-78-88</span>
+                    </div>
+                    <div class="socials">
+                        <a href="#"><i class="fa-brands fa-vk"></i></a>
+                        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+                    </div>
+                </div>
+                <div class="section links col-md-4 cpl-12">
+                    <h3>Быстрые ссылки</h3>
+                    <br>
+                    <ul>
+                        <a href="">
+                            <li>55</li>
+                        </a>
+                        <a href="">
+                            <li>55</li>
+                        </a>
+                        <a href="">
+                            <li>55</li>
+                        </a>
+                        <a href="">
+                            <li>55</li>
+                        </a>
+                    </ul>
+                </div>
+                <div class="section contact-form col-md-4 cpl-12">
+                    <h3>Контакты</h3>
+                    <br>
+                    <form action="index.php" method="post">
+                        <input type="email" name="email" class="text-input contact-input" placeholder="Ваш email адрес...">
+                        <textarea name="message" rows="4" class="text-input contact-input"  placeholder="Ваше сообщение..."></textarea>
+                        <button type="submit" class="btn-big contact-btn">
+                            <i class="fa-solid fa-envelope"></i>
+                            Отправить
+                        </button>
+                    </form>
+                </div>
 
+            </div>
+        </div>
+        <div class="footer-bottom">
+            &copy; myblog.com | designed by romchek6
+        </div>
+    </div>
+    <!-- footer end -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
 <!--    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>-->
 <!--    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.min.js" integrity="sha384-IDwe1+LCz02ROU9k972gdyvl+AESN10+x7tBKgc9I5HFtuNz0wWnPclzo6p9vxnk" crossorigin="anonymous"></script>-->
